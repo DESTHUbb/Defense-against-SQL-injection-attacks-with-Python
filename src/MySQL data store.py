cf5e4c837a988ebb35da3064984c740c89b2b6c1
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-05-01 23:21:58 +0200

    Update MySQL data store.py

--- src/MySQL data store.py
+++ src/MySQL data store.py
@@ -3,3 +3,7 @@ ef execute_stored_procedure(conn, procedure_name, params):
 cursor = conn.cursor()
     placeholders = ', '.join(['%s'] * len(params))
     try:
+cursor.callproc(procedure_name, params)
+        results = cursor.fetchall()
+        return results
+    except 
