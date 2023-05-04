d02dbce5a1e0496f64d1335dc0a6d6a4053debb7
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-05-04 13:07:05 +0200

    Update AI to prevent SQL injection attacks.py

--- src/ai model/AI to prevent SQL injection attacks.py
+++ src/ai model/AI to prevent SQL injection attacks.py
@@ -1,3 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import re
+
+# Establish the connection to the database
+engine = create_engine('postgresql://username:password@localhost:5432/database_name')
