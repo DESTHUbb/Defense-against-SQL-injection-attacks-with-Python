2170166e79729fc5012568f1f1a35a798c503507
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-05-16 00:59:03 +0200

    Update AI to prevent SQL injection attacks.py

--- src/ai model/AI to prevent SQL injection attacks.py
+++ src/ai model/AI to prevent SQL injection attacks.py
@@ -2,16 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import re
 
-# Establish the connection to the database
 engine = create_engine('postgresql://username:password@localhost:5432/database_name')
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Function to clean up user input and prevent SQL injection attacks
 def clean_input(user_input):
 
-  # Remove special characters and escape single quotes
+
     cleaned_input = re.sub('[^A-Za-z0-9]+', '', user_input)
     cleaned_input = cleaned_input.replace("'", "''")
     return cleaned_input
@@ -36,3 +34,5 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgresql://username:password@localhost:5432/database_name')
+
+
